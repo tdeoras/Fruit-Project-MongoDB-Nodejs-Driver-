6331e23b6cb7141b928c7356851808f3daaf0aa2
Tejas <tejasdeoras123@gmail.com>
2019-06-17 20:49:17 -0400

    Mongoose

--- app.js
+++ app.js
@@ -1,54 +1,36 @@
 //jshint esversion:6
 
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+const mongoose = require('mongoose');
 
-// Connection URL
-const url = 'mongodb://localhost:27017';
+mongoose.connect("mongodb://localhost:27017/fruitsDB",{ useNewUrlParser: true });
 
-// Database Name
-const dbName = 'fruitsDB';
+const fruitSchema = new mongoose.Schema ({
 
-// Create a new MongoClient
-const client = new MongoClient(url, { useNewUrlParser: true });
+name : {
+  type: String,
+  required: true
 
-// Use connect method to connect to the Server
-client.connect(function(err) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
+} ,
+rating : {
 
-  const db = client.db(dbName);
+  type : Number,
+  min : 1,
+  max : 10
 
-  findDocuments(db, function() {
-      client.close();
-    });
+},
+review : String
+
+
 });
 
-const insertDocuments = function(db, callback) {
-  // Get the documents collection
-  const collection = db.collection('fruits');
-  // Insert some documents
-  collection.insertMany([
-    {name : "Apple",score : 8,review: "Great fruit"},
-    {name : "Orange",score: 6,review: "Kinda sour"},
-    {name : "Banana", score: 9,review: "Great stuff"}
-  ], function(err, result) {
-    assert.equal(err, null);
-    assert.equal(3, result.result.n);
-    assert.equal(3, result.ops.length);
-    console.log("Inserted 3 documents into the collection");
-    callback(result);
-  });
-};
+const Fruit = mongoose.model("Fruit" , fruitSchema);
 
-const findDocuments = function(db, callback) {
-  // Get the documents collection
-  const collection = db.collection('fruits');
-  // Find some documents
-  collection.find({}).toArray(function(err, fruits) {
-    assert.equal(err, null);
-    console.log("Found the following records");
-    console.log(fruits);
-    callback(fruits);
-  });
-};
+const fruit = new Fruit ({
+
+name : "Apple",
+rating : 8,
+review: "Pretty Solid"
+
+});
+
+//fruit.save();
